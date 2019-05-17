06551bde0aca60a3bc5af92625a5b87d7e1551d7
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-17 08:47:23 -0700

    Adjusting the main app-routing module configuration

--- code/eightsec/src/app/app-routing.module.ts
+++ code/eightsec/src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'clips', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'clips', loadChildren: './clips/clips.module#ClipsPageModule' },
-  { path: 'history', loadChildren: './clips/history/history.module#HistoryPageModule' },
-  { path: 'this-week', loadChildren: './clips/this-week/this-week.module#ThisWeekPageModule' },
-  { path: 'upload', loadChildren: './clips/upload/upload.module#UploadPageModule' },
-  { path: 'view', loadChildren: './clips/view/view.module#ViewPageModule' },
-  { path: 'account', loadChildren: './account/account.module#AccountPageModule' }
+  {
+    path: 'account',
+    loadChildren: './account/account.module#AccountPageModule'
+  }
 ];
 
 @NgModule({
