8f0fb17eda278526434c72f4a94b5e118d522413
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-19 14:23:56 -0700

    Introducing tabs... Error: Cannot find module './clips/history/history.module'

---
+++ code/eightsec/src/app/clips/clips-routing.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClipsPage } from './clips.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: ClipsPage,
+    children: [
+      {
+        path: 'history',
+        children: [
+          {
+            path: '',
+            loadChildren: './clips/history/history.module#HistoryPageModule'
+          },
+          {
+            path: ':clipId',
+            loadChildren: './clips/view/view.module#ViewPageModule'
+          }
+        ]
+      },
+      {
+        path: 'heat',
+        children: [
+          {
+            path: '',
+            loadChildren: './clips/heat/heat.module#HeatPageModule'
+          },
+          {
+            path: ':clipId',
+            loadChildren: './clips/view/view.module#ViewPageModule'
+          }
+        ]
+      },
+      {
+        path: 'upload',
+        children: [
+          {
+            path: '',
+            loadChildren: './clips/upload/upload.module#UploadPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/clips/tabs/history',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/clips/tabs/history',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class ClipsRoutingModule {}
