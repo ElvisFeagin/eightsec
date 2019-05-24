462676591cee08d1ed4e7d8f4259c235006b08c0
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-24 10:15:43 -0700

    Clean up the app-routing module to include only the top level navigation paths.

--- code/eightsec/src/app/app-routing.module.ts
+++ code/eightsec/src/app/app-routing.module.ts
@@ -4,13 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'clips', pathMatch: 'full' },
   { path: 'clips', loadChildren: './clips/clips.module#ClipsPageModule' },
-  { path: 'view', loadChildren: './clips/view/view.module#ViewPageModule' },
-  {
-    path: 'upload',
-    loadChildren: './clips/upload/upload.module#UploadPageModule'
-  },
-  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
-  { path: 'help', loadChildren: './help/help.module#HelpPageModule' }
+  { path: 'help', loadChildren: './help/help.module#HelpPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' }
 ];
 
 @NgModule({
