79c5d1f6f05037b334750955a7f54f84423b5120
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-28 10:48:53 -0700

    Expand the clip model, and fully display 3 dummy clips info on the home page.

--- code/eightsec/src/app/models/clip.model.ts
+++ code/eightsec/src/app/models/clip.model.ts
@@ -1,3 +1,11 @@
 export class Clip {
-  constructor(public id: string, public caption: string) {}
+  constructor(
+    public id: string,
+    public weekNumber: number,
+    public weekDate: Date,
+    public nickname: string,
+    public votes: number,
+    public caption: string,
+    public imageUrl: string
+  ) {}
 }
--- code/eightsec/src/app/services/clips.service.ts
+++ code/eightsec/src/app/services/clips.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { Clip } from '../models/clip.model';
 
 @Injectable({
@@ -7,16 +8,31 @@ import { Clip } from '../models/clip.model';
 export class ClipsService {
   private _clips: Clip[] = [
     new Clip(
-      'c1',
-      'But, like most politicians, he promised more than he could deliver. I can'
+      'c3',
+      4,
+      new Date(2025, 6, 27),
+      'MrSkeleton',
+      23,
+      'But, like most politicians, he promised more than he could deliver. I can',
+      'https://free-images.com/lg/7c29/cat_pet_mirror_697113.jpg'
     ),
     new Clip(
       'c2',
-      'Please, Don-Bot... look into your hard drive, and open your mercy file!'
+      3,
+      new Date(2025, 6, 20),
+      'Filet Minyon',
+      10,
+      'Please, Don-Bot... look into your hard drive, and open your mercy file!',
+      'https://free-images.com/lg/3427/clown_claw_circus_tihany.jpg'
     ),
     new Clip(
-      'c3',
-      'I was having the most wonderful dream. Except you were there'
+      'c1',
+      2,
+      new Date(2025, 6, 13),
+      'P. Ennis',
+      3,
+      'I was having the most wonderful dream. Except you were there',
+      'https://free-images.com/lg/ea39/boys_funny_faces_expressions.jpg'
     )
   ];
 
