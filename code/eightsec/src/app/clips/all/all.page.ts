7218aa6e12eebffa904af768030495da1577b8f2
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-06-01 15:14:09 -0700

    Displaying all clips on the 'home' page component.

--- code/eightsec/src/app/clips/all/all.page.ts
+++ code/eightsec/src/app/clips/all/all.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ClipsService } from 'src/app/services/clips.service';
+import { Clip } from 'src/app/models/clip.model';
+
 @Component({
   selector: 'app-all',
   templateUrl: './all.page.html',
   styleUrls: ['./all.page.scss'],
 })
 export class AllPage implements OnInit {
+  loadedClips: Clip[] = [];
 
-  constructor() { }
+  constructor(private clipsService: ClipsService) { }
 
   ngOnInit() {
+    this.loadedClips = this.clipsService.clips;
   }
 
 }
