c17780981cb73fa3e2b9070d9d18f3652ebcc865
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-06-02 08:10:31 -0700

    Set up forward navigation for the 'heat' and 'past' pages

--- code/eightsec/src/app/clips/heat/heat.module.ts
+++ code/eightsec/src/app/clips/heat/heat.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HeatPage } from './heat.page';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HeatPage
+  }
+];
+
 @NgModule({
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, RouterModule.forChild(routes)],
   declarations: [HeatPage]
 })
-export class HeatPageModule {
-  constructor() {
-    console.log('IN: HeatPageModule -> constructor');
-  }
-}
+export class HeatPageModule {}
