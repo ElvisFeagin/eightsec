d9b827d9ff33344438f6063c279497f2569f27f2
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-06-26 09:31:56 -0700

    Generate the auth page/folder to be used when the user should authenticate to proceed further

--- code/eightsec/src/app/app-routing.module.ts
+++ code/eightsec/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'clips', pathMatch: 'full' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
-  { path: 'clips', loadChildren: './clips/clips.module#ClipsPageModule' }
+  { path: 'clips', loadChildren: './clips/clips.module#ClipsPageModule' },
 ];
 
 @NgModule({
